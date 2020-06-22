970c1e7827c1de529cb1e72ea54c5ed501449b98
Angel <55843433+angelvb114@users.noreply.github.com>
2020-06-22 18:00:19 -0500

    feat: add test for all rest service

--- src/test/java/com/data/pipeline/RestTest.java
+++ src/test/java/com/data/pipeline/RestTest.java
@@ -13,6 +13,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
+ * Clase encargada de probar los servicios rest
+ *
  * @author Angel Vega
  * @version: 1.0
  * Created on junio 22, 2020
@@ -29,10 +31,36 @@ public class RestTest extends DataPipelineApplicationTests {
     public void getFindAllAlcaldiasTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders
                 .get("/metrobus/location/alcaldias").accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.alcaldias_disponibles").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.alcaldias_disponibles").isNotEmpty());
+    }
+
+    @Test
+    public void getFindAllMetrobusLocationTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .get("/metrobus/location/units").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.unidades_disponibles").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.unidades_disponibles").isNotEmpty());
+    }
+
+    @Test
+    public void getFindMetrobusLocationByAlcaldiaTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .get("/metrobus/location/alcaldias/6").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.Iztacalco").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.Iztacalco").isNotEmpty());
+    }
+
+    @Test
+    public void getFindLocationDataByUnitTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .get("/metrobus/location/units/170").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.history_data").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.history_data").isNotEmpty());
     }
 
 }
